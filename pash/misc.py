a328ff4c25319770cc07413976c8d23ad1f8b7da
Matthias Monschein <monschein.matthias@gmail.com>
2020-04-09 18:35:44 +0200

    added typing for misc.

--- pash/misc.py
+++ pash/misc.py
@@ -2,21 +2,21 @@ import shutil, random
 import colorama as cr
 cr.init()
 
-def center(txt):
+def center(txt: str) -> str:
     w = shutil.get_terminal_size().columns
     mx = max([len(l) for l in txt.split('\n')])
     return '\n'.join([' '*((w-mx)//2)+l for l in txt.split('\n')])
 
-def print_center(txt):
+def print_center(txt: str) -> None:
     print(center(txt))
 
-def printc(txt):
+def printc(txt: str) -> None:
     """
         Alias for `print_center(txt)`
     """
     print_center(txt)
 
-def fancy_print(txt):
+def fancy_print(txt: str) -> None:
     txt = center(txt)
     col = [ cr.Fore.LIGHTRED_EX, cr.Fore.LIGHTGREEN_EX, cr.Fore.LIGHTBLUE_EX, cr.Fore.LIGHTBLACK_EX, cr.Fore.LIGHTYELLOW_EX, cr.Fore.LIGHTWHITE_EX, cr.Fore.LIGHTCYAN_EX, cr.Fore.LIGHTMAGENTA_EX, ]
     fin = ''
@@ -27,7 +27,7 @@ def fancy_print(txt):
         txt = txt[r:]
     print(fin+cr.Fore.RESET)
 
-def fprint(txt):
+def fprint(txt: str) -> None:
     """
         Alias for `fancy_print(txt)`
     """
