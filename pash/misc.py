077bb724a1d887d235e33c4e10b3baf3b1715976
Matthias Monschein <monschein.matthias@gmail.com>
2020-04-16 11:38:30 +0200

    added progressbar

--- pash/misc.py
+++ pash/misc.py
@@ -52,3 +52,83 @@ def print_table(tab: List[List[str]], align: TALIGN = TALIGN.RIGHT) -> None:
     ms = [max(c)+1 for c in zip(*[[len(x) for x in r] for r in tab])]
     for r in tab:
         print(' '+' '.join([('{:'+('>' if align == TALIGN.RIGHT else '')+str(ms[i])+'s}').format(c) for i, c in enumerate(r)]))
+
+class ProgressBar(object):
+    """
+    Represents a cli progress bar.
+
+    ...
+
+    Attributes
+    ----------
+    max : int
+        Maximum value.
+    title : str
+        The progress bar's title.
+    val : float
+        The bar's current value.
+    w : int
+        The bar's width in the console [characters].
+    done : bool
+        Whether or not, the process has finished.
+    
+    Methods
+    -------
+    update(val)
+        Make some progress!
+    inc(by)
+        Increment the value by [by].
+    end()
+        End the process.
+    ensure_end()
+        Calls end, if the process hasn't ended according to the progress bar. 
+        Should always be called to make sure the progress bar finishes.
+    __refresh()
+        Refresh/redraw the progress bar.
+    """
+
+    def __init__(self, max: int, title: str = '', val: float = 0.) -> None:
+        """
+        Parameters
+        ----------
+        max : int
+            The maximum value.
+        title : str
+            The title. (optional)
+        val : float
+            The starting value. (optional)
+        """
+        self.max: int   = max
+        self.title: str = title
+        self.val: float = val
+        self.w: int     = 20
+        self.done: bool = False
+        self.__refresh()
+
+    def update(self, val: float) -> None:
+        """Sets the current value to the given value"""
+        if self.done:
+            return
+        self.val = val
+        self.__refresh()
+        if self.val >= self.max:
+            self.end()
+
+    def inc(self, by: float) -> None:
+        """Increments the current value by the given value."""
+        self.update(self.val+by)
+
+    def end(self) -> None:
+        """Ends the progress bar."""
+        self.done = True
+        print('\r {} ['.format(self.title)+'='*self.w+'] 100.00% ... ')
+
+    def ensure_end(self) -> None:
+        """Ensures the progress bar has ended/ends. (always call when finished!)"""
+        if not self.done:
+            self.end()
+
+    def __refresh(self) -> None:
+        """Refreshes the progress bar."""
+        pc = int((self.val/self.max)*100)
+        print('\r {} ['.format(self.title)+'='*(pc//(100//self.w))+'-'*(self.w-pc//(100//self.w))+'] {:6.2f}% ...'.format(pc), end='')
